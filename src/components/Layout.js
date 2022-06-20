332bec615d19a3ce62da1b4439c4f692107bac6b
Mariusz <mariusz.janowski@yahoo.pl>
2022-06-20 19:18:01 +0200

    new-commit

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,5 @@
 import * as React from "react"
 import Header from "./Header"
-import styled from "styled-components"
-import { gatsby, useStaticQuery, graphql } from "gatsby"
-import BackgroundSlider from "gatsby-image-background-slider"
 
 const Layout = ({ children }) => {
   return (
